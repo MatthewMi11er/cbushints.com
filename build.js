5fccd378330fa86c0f9f9c6722e8f8bc018bb7b4
Matthew Miller <matthew@mi11er.net>
2017-01-28 18:51:46 -0500

    feat(site): Set site to 'comming soon'
    Create a base layout for the site using pug templeting and scss.

--- build.js
+++ build.js
@@ -1,17 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 
+const layouts = require('metalsmith-layouts');
 const Metalsmith = require('metalsmith');
+const sass = require('metalsmith-sass');
 
 Metalsmith(__dirname)
   .metadata({
     site: {
-      title: 'cbushints.com',
+      description: '',
+      title: 'CBusHints',
       url: 'https://cbushints.com',
     },
   })
-  .source('./src')
+  .source('./src/content')
   .destination('./dist')
+  .use(layouts({
+    directory: './src/pug',
+    engine: 'pug',
+    rename: true,
+  }))
+  .use(sass({
+    outputDir: originalPath => originalPath.replace("scss", "css"),
+  }))
   .build((err) => {
     if (err) {
       throw err;
